f30a11777ed54ff172343e89ede752707c5e3841
Chris Forstall <cforstall@gmail.com>
2015-10-28 07:03:17 +0100

    add big lda test

--- src/val_flac.R
+++ src/val_flac.R
@@ -452,17 +452,39 @@ randscores.topics.k.effective.nobs <- sapply(table(kmcl.topics.k.effective), cho
 
 # topics test
 
-one.lda.per.kmeans <- function(ntopics, nclusters) {
+one.lda.per.kmeans <- function(ntopics, nclusters, nreps=10, ncores=NA) {
 
-  t0 <- Sys.time()
-  on.exit(
-    cat(
-      paste(" - [", ntopics, " topics; ", nclusters, " clusters]", sep=""),
-      "...",
-      difftime(Sys.time(), t0, units = "min"),
-      "minutes\n"
+  inner.function <- function(i) {
+    t0 <- Sys.time()
+    on.exit(
+      cat(
+        paste(" - [", i, "/", nreps, "]"),
+        "...",
+        difftime(Sys.time(), t0, units = "min"),
+        "minutes\n"
+      )
     )
-  )
-  
-  kmeans(slot(LDA(dtm.tf, k = ntopics), "gamma"), nclusters)$cluster
+    kmeans(slot(LDA(dtm.tf, k = ntopics), "gamma"), nclusters)$cluster
+  }
+
+  cat("Generating", nreps, "reps with", ntopics, "topics and", nclusters, "classes\n")
+
+  if(is.na(ncores)) {
+    return(do.call(cbind, lapply(1:nreps, inner.function)))
+  } else {
+    return(do.call(cbind, mclapply(1:nreps, inner.function, mc.cores = ncores)))
+  } 
 }
+
+output.dir <- "~/ldatest"
+if(! dir.exists(output.dir)) {
+	dir.create(output.dir, recursive=T)
+}
+
+set.seed(11011981)
+
+lapply(2:3, function(nclusters) {
+   output.file <- file.path(output.dir, paste(ntopics, "-", nclusters, ".txt", sep=""))
+	write.table(output.file, one.lda.per.kmeans(ntopics, nclusters, nreps = 15, ncores))
+}
+
